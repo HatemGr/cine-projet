0142f00ba78a93725c2dd4626ad5520384b5c32f
Hatem GRIBI <hatemgribi@gmail.com>
2023-01-05 08:50:18 +0100

    initialize backend

---
+++ backend/projetcine/src/main/java/com/projet/cine/projetcine/ProjetcineApplication.java
@@ -0,0 +1,13 @@
+package com.projet.cine.projetcine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetcineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetcineApplication.class, args);
+	}
+
+}
---
+++ backend/projetcine/src/test/java/com/projet/cine/projetcine/ProjetcineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.cine.projetcine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetcineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
